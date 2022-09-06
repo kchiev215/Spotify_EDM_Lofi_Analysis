a557666e911ad45343965e0674fe6c9093d170ec
Thina <kchiev215@gmail.com>
2022-09-06 15:54:26 -0400

    added to website

--- Flask/application/__init__.py
+++ Flask/application/__init__.py
@@ -2,37 +2,20 @@ from flask import render_template, url_for
 import json
 import plotly
 import plotly.express as px
-from flaskext.markdown import Markdown
 from flask import Flask
+import os
 
 app = Flask(__name__)
-Markdown(app)
+
 
 @app.route("/")
 def index():
-    mkd_text = "## Your Markdown Here "
-    return render_template("index.html",mkd_text=mkd_text)
+    return render_template("index.html")
 
 
 @app.route('/charts')
 def chart1():
-    # Graph One
-    df = px.data.tips()
-    fig = px.histogram(df, x="total_bill", y="tip", color="sex", marginal="rug", hover_data=df.columns)
-    graph1JSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-
-    # Graph two
-    df = px.data.iris()
-    fig2 = px.scatter_3d(df, x='sepal_length', y='sepal_width', z='petal_width',
-                         color='species', title="Iris Dataset")
-    graph2JSON = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)
-
-    # Graph three
-    df = px.data.gapminder().query("continent=='Oceania'")
-    fig3 = px.line(df, x="year", y="lifeExp", color='country', title="Life Expectancy")
-    graph3JSON = json.dumps(fig3, cls=plotly.utils.PlotlyJSONEncoder)
-
-    return render_template('charts.html', graph1JSON=graph1JSON, graph2JSON=graph2JSON, graph3JSON=graph3JSON)
+    return render_template('charts.html')
 
 
 @app.route("/introduction")
